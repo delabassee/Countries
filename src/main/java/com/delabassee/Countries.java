c80a5c523ea2ce1165a22f1d85a317c931060746
David Delabassee <david.delabassee@gmail.com>
2018-07-31 14:54:41 +0200

    initial commit

---
+++ src/main/java/com/delabassee/Countries.java
@@ -0,0 +1,86 @@
+package com.delabassee;
+
+import java.util.*;
+
+public class Countries {
+
+    static class Country {
+        private String iso;
+        private String code;
+        private String name;
+
+        Country(String iso, String code, String name) {
+            this.iso = iso;
+            this.code = code;
+            this.name = name;
+        }
+
+        public String getIso() {
+            return iso;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String toString() {
+            return iso + " - " + code + " - " + name;
+        }
+
+    }
+
+
+    public static List<Country> getAll() {
+
+        List<Country> countries = new ArrayList<Country>();
+        String[] isoCountries = Locale.getISOCountries();
+
+        for (String country : isoCountries) {
+            Locale locale = new Locale("en", country);
+            String iso = locale.getISO3Country();
+            String code = locale.getCountry();
+            String name = locale.getDisplayCountry();
+
+            if (!"".equals(iso) && !"".equals(code) && !"".equals(name)) {
+                countries.add(new Country(iso, code, name));
+            }
+        }
+
+        return countries;
+    }
+
+
+    public static String getName(String countryCode) {
+
+        String[] isoCountries = Locale.getISOCountries();
+        String upperCode = countryCode.toUpperCase();
+
+        for (String country : isoCountries) {
+            Locale locale = new Locale("en", country);
+            if (country.equals(upperCode)) {
+                return locale.getDisplayCountry();
+            }
+        }
+        return "not found";
+    }
+
+
+    public static String getCode(String countryName) {
+
+        String[] isoCountries = Locale.getISOCountries();
+        String upperName = countryName.toUpperCase();
+
+        for (String country : isoCountries) {
+            Locale locale = new Locale("en", country);
+            if (locale.getDisplayCountry().toUpperCase().equals(upperName)) {
+                return locale.getCountry();
+            }
+        }
+        return "not found";
+    }
+
+}
